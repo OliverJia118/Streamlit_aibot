93fa6ff2d4b1903befab49ee7706590e2d23a578
Oliver Jia” <ojsumeng@163.com>
2023-06-29 17:50:52 +0800

    Test round 2

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ else:
 openai_api_key = st.secrets['OPENAI_KEY']
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "user", "content": "担担面将如何帮助您呢？"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -70,8 +70,14 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    st.write(response)
-    msg = response.choices[0].message
+    # response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = zhipuai.model_api.invoke(
+                model='chatglm_6b',
+                prompt=st.session_state.messages,
+                temperature=0.9,
+                top_p=0.7,
+                incremental=True
+            )
+    msg = response['data']['choices'][0]['content']
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
