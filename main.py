1e636bdd0d50c2aac4b79925c9b8f2700e3f3371
Oliver Jia” <ojsumeng@163.com>
2023-06-29 16:01:16 +0800

    Test round 2

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import streamlit as st
 import zhipuai
 
 
-st.write('Streamlit')
-print('test')
+st.title('DDM AI Chatbox Version 1.0')
 
-st.title('Testing for update')
+function = st.sidebar.selectbox('Function', ['Password', 'API'])
+if function == 'Password':
+    password = st.sidebar.text_input('Password', type='password')
+    if password == '123456':
+        st.title('DDM AI Chatbox Version 1.0')
+        st.write('Welcome to DDM AI Chatbox Version 1.0')
+        st.write('')
 
-st.write('WHy')
 
 
