1c538429f976561b3548ce72ec82e108068e4c08
Oliver Jia” <ojsumeng@163.com>
2023-06-29 16:23:48 +0800

    Test round 2

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ function = st.sidebar.selectbox('Function', ['Password', 'API'])
 if function == 'Password':
     password = st.sidebar.text_input('Password', type='password')
     if password == 'dandanzuibang':
+        zhipuai.api_key = '14c95ca09607c9c512335cb8f14bce9f.hiDwX89fjf04hpwM'
         st.title('Welcome to DDM AI Chat box Version 1.0')
         st.write('')
     elif password == '':
@@ -25,14 +26,24 @@ if function == 'Password':
 else:
     api = st.sidebar.text_input('API', type='text')
     if api != '':
+        zhipuai.api_key = api
         pass
     else:
         st.warning('API is empty')
         st.stop()
 
 
+test_prompt = st.text_input('Test prompt', type='text')
 
+@st.cache_data
+def ai_test(prompt):
+    response = zhipuai.model_api.invoke(
+        model = 'chatglm_6b',
+        prompt = [{'role':'user', 'content':prompt}]
+    )
 
+    return response
 
-
+response = ai_test(test_prompt)
+st.write(response['data']['choices'][0]['content'])
 
