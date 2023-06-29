c871e1b5bf95ea6e2daffe1cd053d8539e04a318
Oliver Jia” <ojsumeng@163.com>
2023-06-29 17:41:07 +0800

    Test round 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import zhipuai
 import warnings
+import openai
 
 warnings.filterwarnings("ignore")
 st.set_page_config(layout='wide', page_icon='🐶', page_title='担担面AI Chat box')
@@ -31,26 +32,45 @@ else:
         st.warning('API is empty')
         st.stop()
 
-test_prompt = st.text_input('担担面你好！')
+# test_prompt = st.text_input('担担面你好！')
+#
+#
+# @st.cache_data
+# def ai_test(prompt):
+#     response = zhipuai.model_api.invoke(
+#         model='chatglm_6b',
+#         prompt=[{'role': 'user', 'content': prompt}],
+#         temperature=0.9,
+#         top_p=0.7,
+#         incremental=True
+#     )
+#
+#     return response
+#
+#
+# if test_prompt != '':
+#     response = ai_test(test_prompt)
+# else:
+#     st.warning('Text is empty')
+#     st.stop()
+# # response = ai_test(test_prompt)
+# st.write(response['data']['choices'][0]['content'])
 
 
-@st.cache_data
-def ai_test(prompt):
-    response = zhipuai.model_api.invoke(
-        model='chatglm_6b',
-        prompt=[{'role': 'user', 'content': prompt}],
-        temperature=0.9,
-        top_p=0.7,
-        incremental=True
-    )
+openai_api_key = st.secrets('OPENAI_KEY')
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-    return response
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
+if prompt := st.chat_input():
 
-if test_prompt != '':
-    response = ai_test(test_prompt)
-else:
-    st.warning('Text is empty')
-    st.stop()
-# response = ai_test(test_prompt)
-st.write(response['data']['choices'][0]['content'])
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
