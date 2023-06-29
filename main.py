c0f046ea7d8af286e6433b82f87e5b7266835a8b
Oliver Jia” <ojsumeng@163.com>
2023-06-29 18:06:59 +0800

    Test round 2

--- main.py
+++ main.py
@@ -80,8 +80,7 @@ if prompt := st.chat_input():
         model='chatglm_6b',
         prompt=[st.session_state.messages],
         temperature=0.9,
-        top_p=0.7,
-        incremental=True
+        top_p=0.7
     )
 
     st.write(response)
