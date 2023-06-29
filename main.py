716297096a8db0ff5e473a3ba5ad000531f4d642
Oliver Jia” <ojsumeng@163.com>
2023-06-29 17:57:12 +0800

    Test round 2

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ else:
 #
 # @st.cache_data
 # def ai_test(prompt):
-#     response = zhipuai.model_api.invoke(
-#         model='chatglm_6b',
-#         prompt=[{'role': 'user', 'content': prompt}],
-#         temperature=0.9,
-#         top_p=0.7,
-#         incremental=True
-#     )
+    response = zhipuai.model_api.invoke(
+        model='chatglm_6b',
+        prompt=[{'role': 'user', 'content': prompt}],
+        temperature=0.9,
+        top_p=0.7,
+        incremental=True
+    )
 #
 #     return response
 #
@@ -70,9 +70,16 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-
-    msg = response.choices[0].message
-    st.write(msg)
+    # response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = zhipuai.model_api.invoke(
+        model='chatglm_6b',
+        prompt=st.session_state.messages,
+        temperature=0.9,
+        top_p=0.7,
+        incremental=True
+    )
+    # msg = response.choices[0].message
+    # st.write(msg)
+    msg = response['data']['choices'][0]
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
