49edab2912da423947ab692d6d3eec6d9f91f402
Oliver Jia” <ojsumeng@163.com>
2023-06-30 14:32:30 +0800

    Test round 2

--- main.py
+++ main.py
@@ -84,5 +84,5 @@ if prompt := st.chat_input():
     msg = response['data']['choices'][0]
     # st.write('zhipu',msg)
     # st.session_state.messages.append(msg)
-    # st.write(msg.content)
+    st.write(msg)
     st.chat_message("assistant").write(msg.content)
