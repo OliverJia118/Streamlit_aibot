887f3b1b4d7f741f9a1dcf6ada27d2cd7b0fdd85
Oliver Jia” <ojsumeng@163.com>
2023-06-30 16:20:43 +0800

    Test round 2

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ if prompt := st.chat_input():
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=st.session_state.messages,
                                             temperature=0.9,
-                                            max_tokens=500)
+                                            max_tokens=1500)
     msg = response.choices[0].message
 
     st.session_state.messages.append(msg)
