a07abb355892147961a4c4ca7f6f860e0f82d3fd
Oliver Jia” <ojsumeng@163.com>
2023-06-30 16:47:53 +0800

    Test round 2

--- main.py
+++ main.py
@@ -31,10 +31,13 @@ else:
         st.warning('API is empty')
         st.stop()
 
+
+st.header("💬欢迎呀，这里是AI担担面")
 style = st.selectbox('你希望担担面是什么性格呢？', ['诙谐幽默', '严肃认真', '学术专业', '悲观','乐观'])
 
 personality = '。回答问题时要求如下：将自己模仿成一个名字叫 ”担担面“ 的人来回答， 回答风格要求：' + str(style)
-st.header("💬欢迎呀，这里是AI担担面")
+temperature = st.radio_slider('离谱程度（越高越离谱）', min_value=0.0, max_value=2, value=0.9)
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "担担面有何可为您效劳?"}]
 
@@ -48,7 +51,7 @@ if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=st.session_state.messages,
-                                            temperature=0.9)
+                                            temperature=temperature)
     msg = response.choices[0].message
 
     st.session_state.messages.append(msg)
